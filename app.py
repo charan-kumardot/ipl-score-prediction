3e5204bcd5331838523fa2983bd5dc51bd165f4e
charan-kumardot <59021375+charan-kumardot@users.noreply.github.com>
2020-09-14 12:06:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
-import streamlit.components.v1 as components
 filename = 'linear_model.pkl'
 regressor = pkl.load(open(filename, 'rb'))
 st.title('IPL 2020')
